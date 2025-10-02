97e597bf34f217c12f20c3262d40ea9089b60ccb
GSC-GG <gui.cavalcante250@gmail.com>
2025-10-02 19:00:21 -0300

    Adição do Swagger

--- src/main/java/br/ifsp/ContactsApiApplication.java
+++ src/main/java/br/ifsp/ContactsApiApplication.java
@@ -2,12 +2,14 @@ package br.ifsp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 
+@OpenAPIDefinition(info = @Info(title = "Contacts API", version = "1.0", description = "Documentação da API de Contatos"))
 @SpringBootApplication
 public class ContactsApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ContactsApiApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(ContactsApiApplication.class, args);
+    }
 }
